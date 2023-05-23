628ba1bba1ee198177425277d3d723a41bbbf90c
markus <datamastery87@gmail.com>
2023-05-23 20:43:04 +0200

    add embeddings

--- voice.py
+++ voice.py
@@ -1,5 +1,17 @@
-import os
 import openai
+from dotenv import load_dotenv
+import os
 
-audio_file = open("german.m4a", "rb")
+load_dotenv()
+
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
+audio_file = open("sample.m4a", "rb")
 transcript = openai.Audio.translate("whisper-1", audio_file)
+print(transcript)
+print(transcript["text"])
+
+
+audio_file= open("sample.m4a", "rb")
+transcript = openai.Audio.transcribe("whisper-1", audio_file)
+print(transcript["text"])
